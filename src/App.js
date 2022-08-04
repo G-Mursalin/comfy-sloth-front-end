0420801fb59cc2d94d686e8609e1d5ead1579500
Golam Mursalin <golammursalin309@gmail.com>
2022-08-04 08:54:48 +0600

    project initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+// React Router
+import { Routes, Route, Link } from "react-router-dom";
+// Components
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Products from "./components/Products/Products";
+import SingleProduct from "./components/Products/SingleProduct";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+      </Routes>
     </div>
   );
 }
